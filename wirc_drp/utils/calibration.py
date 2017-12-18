624889990c7b7748905d7957f0fe448c4ffae0b5
Kaew Tinyanont <st@astro.caltech.edu>
2017-12-18 04:35:03 -0800

    small bug

--- wirc_drp/utils/calibration.py
+++ wirc_drp/utils/calibration.py
@@ -151,7 +151,7 @@ def masterFlat(flat_list, master_dark_fname, normalize = 'median', local_sig_bad
         #read in the existing bp map
         hdu = f.open(hotp_map_fname)
         #hdu[0].data += np.array(bad_px.mask, dtype=float)
-        hdu[0].data = np.logical_or(hdu[0].data.astype('bool'), bad_px) #use logical or to combine bad pixel maps
+        hdu[0].data = np.logical_or(hdu[0].data.astype(bool), bad_px) #use logical or to combine bad pixel maps
         bp_outname = flat_list[-1].rsplit('.',1)[0]+"_bp_map.fits"
 
     else: 
