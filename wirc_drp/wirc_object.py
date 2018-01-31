fbf36cad2ccb2609c4a1846434698e4008f4af00
Shreyas Vissapragada <svissapr@caltech.edu>
2018-01-31 14:31:40 -0800

    bkg frame will not have calbrted in header if not reduced

--- wirc_drp/wirc_object.py
+++ wirc_drp/wirc_object.py
@@ -90,7 +90,7 @@ class wirc_data(object):
 
             self.calibrated = False
             self.bkg_subbed = False
-
+            
             self.n_sources = 0
             self.source_list = []
             self.dark_fn = dark_fn        
@@ -187,7 +187,10 @@ class wirc_data(object):
                 background = background_hdu[0].data
                 bkg_exp_time = background_hdu[0].header["EXPTIME"]*background_hdu[0].header["COADDS"]
                 #Check if background is already reduced
-                bkg_reduced = background_hdu[0].header["CALBRTED"]
+                try:
+                    bkg_reduced = background_hdu[0].header["CALBRTED"]
+                except KeyError as e:
+                    bkg_reduced = False
 
                 if bkg_reduced == False:
                     #Checking Dark Exposure times and scaling if need be
