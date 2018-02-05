93c321a76922be18fa31314bb949817d67b5367d
Samaporn Tinyanont <st@astro.caltech.edu>
2018-02-05 11:27:58 -0800

    debugging plotting

--- wirc_drp/utils/spec_utils.py
+++ wirc_drp/utils/spec_utils.py
@@ -458,20 +458,20 @@ def optimal_extraction(data, background, extraction_range, bad_pixel_mask = None
         
         #optimized variance 
         variance_opt = (read_out_noise/gain)**2 + (flux_0*P_0 + background)/gain
-
+        print('Extraction range is', extraction_range)
         if plot: #what are relavent diagnostic plots from here? P image is one, the actual extraction range is another. 
             fig, ax = plt.subplots(1,3,figsize = (15,5))
             ax[0].imshow(data - background, origin = 'lower')
-            ax[0].plot([0,1],[extraction_range[0],extraction_range[0]], '-')
-            ax[0].plot([0,1],[extraction_range[1],extraction_range[1]], '-')
+            ax[0].plot([0,data.shape[1]],[extraction_range[0],extraction_range[0]], '-')
+            ax[0].plot([0,data.shape[1]],[extraction_range[1],extraction_range[1]], '-')
             ax[0].set_title('Data - background')
             ax[1].imshow(variance_opt, origin = 'lower')
-            ax[1].plot([0,1],[extraction_range[0],extraction_range[0]], '-')
-            ax[1].plot([0,1],[extraction_range[1],extraction_range[1]], '-')
+            ax[1].plot([0,data.shape[1]],[extraction_range[0],extraction_range[0]], '-')
+            ax[1].plot([0,data.shape[1]],[extraction_range[1],extraction_range[1]], '-')
             ax[1].set_title('Optimized variance')
             ax[2].imshow(P_0, origin = 'lower')
-            ax[2].plot([0,1],[extraction_range[0],extraction_range[0]], '-')
-            ax[2].plot([0,1],[extraction_range[1],extraction_range[1]], '-')
+            ax[2].plot([0,data.shape[1]],[extraction_range[0],extraction_range[0]], '-')
+            ax[2].plot([0,data.shape[1]],[extraction_range[1],extraction_range[1]], '-')
             ax[2].set_title('Profile image')
             # for i in range(extraction_range[0], extraction_range[1]):
             #     plt.plot(P_0[i,:])
