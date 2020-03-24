73cecbddf57d2fd4949776134834057c4f80bb62
maxwellmb <maxmb.astro@gmail.com>
2020-03-24 14:50:59 -0700

    New pre-commit

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+GIT_DIR=$(git rev-parse --git-dir)
+
 echo "Running pre-commit hook\n"
 
 echo "Starting Test 1"
-python ./scripts/test1.py
+python tests/test1.py
 
 if [ $? -ne 0 ]; then
  echo "Pre-commit tests failed on test 1"
